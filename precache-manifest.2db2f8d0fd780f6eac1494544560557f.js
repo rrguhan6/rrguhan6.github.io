8a5c1a87cc8001912ae1e5d5fe31797bc0c0cd7d
rrguhan6 <63386495+rrguhan6@users.noreply.github.com>
2020-09-26 17:38:09 +0530

    Add files via upload

---
+++ precache-manifest.2db2f8d0fd780f6eac1494544560557f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8015747a3cdbcd12c98939d736518b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba20d9bbb26b6381e253",
+    "url": "/static/css/main.742b2e8f.chunk.css"
+  },
+  {
+    "revision": "169091e310d5980e3a82",
+    "url": "/static/js/2.e9d16fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9d16fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba20d9bbb26b6381e253",
+    "url": "/static/js/main.ed4014a2.chunk.js"
+  },
+  {
+    "revision": "85a12c31901e373959c2",
+    "url": "/static/js/runtime-main.a544320e.js"
+  }
+]);
